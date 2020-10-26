672af8d630e689a4e103bf7d6108f4573f84e191
CS-Lawrencium <928397656@qq.com>
2020-10-26 11:22:26 +0800

    git push

---
+++ PerfectNumber.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+int GetPerfect(int x);
+int main()
+{
+    int n;
+    printf("Please input n:");
+    scanf("%d", &n);
+    if (GetPerfect(n))
+    {
+        printf("Yes!\n");
+    }
+    else
+    {
+        printf("No!\n");
+    }
+    return 0;
+}
+int GetPerfect(int x)
+{
+    int i, sum = 0;
+    for (i = 1; i < x;i++)
+    {
+        if(x%i==0)
+        {
+            sum = sum + i;
+        }
+    }
+    return sum == x ? i : 0;
+}

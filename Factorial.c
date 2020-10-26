04a89121e62e55eab631cd4813d9df764a837b31
CS-Lawrencium <928397656@qq.com>
2020-10-26 15:17:11 +0800

    git push

---
+++ Factorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+long Factorial(int x);
+int main()
+{
+    int x;
+    while (1)
+    {
+        printf("input x:");
+        scanf("%d", &x);
+        if (x <= -1)
+            exit(0);
+        else
+            printf("%d! = %d\n", x, Factorial(x));
+    }
+    return 0;
+}
+long Factorial(int x)
+{
+    int i;
+    int result = 1;
+    for (i = 2; i <= x; i++)
+    {
+        result *= i;
+    }
+    return result;
+}

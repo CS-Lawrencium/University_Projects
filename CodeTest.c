0db47e52da9a60c0d1aa04efea2bfa60d57f3343
CS-Lawrencium <928397656@qq.com>
2020-10-29 20:17:57 +0800

    冲

--- CodeTest.c
+++ CodeTest.c
@@ -1,31 +1,6 @@
 #include <stdio.h>
-void Hnt(int n, char a, char b, char c);
-int count;
 int main()
 {
-    count = 1;
-    int n;
-
-    printf("Please enter the number of discs:");
-    scanf("%d", &n);
-    Hnt(n, 'a', 'b', 'c');
-    printf("\tTotal:%d\n", count);
-    return 0;
-}
-
-void Hnt(int n, char a, char b, char c)
-{
-
-    if (n == 1)
-    {
-        printf("%2d-(%2d):%c==>%c\n", count, n, a, b);
-        count++;
-    }
-    else
-    {
-        Hnt(n - 1, a, c, b);
-        printf("%2d-(%2d):%c==>%c\n", count, n, a, c);
-        Hnt(n - 1, b, a, c);
-        count++;
-    }
+    int a, b, c;
+    printf("%d+%d+%c", a, b, c);
 }
---
+++ opreator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int data1, data2;
+    char op;
+    printf("Please enter an expression:");
+    scanf("%d%c%d", &data1, &op, &data2);
+    switch (op)
+    {
+    case '+':
+        printf("%d + %d = %d \n", data1, data2, data1 + data2);
+        break;
+    case '-':
+        printf("%d - %d = %d \n", data1, data2, data1 - data2);
+        break;
+    case '*':
+        printf("%d * %d = %d \n", data1, data2, data1 * data2);
+        break;
+    case '/':
+        if (data2 == 0)
+            printf("Division by zero!\n");
+        else
+            printf("%d / %d = %d \n", data1, data2, data1 / data2);
+        break;
+    default:
+        printf("Invalid operator! \n");
+    }
+    return 0;
+}

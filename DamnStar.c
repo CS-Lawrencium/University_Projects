221417e26d0be2cec246fd86020046f9dff330d1
CS-Lawrencium <928397656@qq.com>
2020-10-30 14:21:26 +0800

    一道阴间题目

---
+++ DamnStar.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n = 4;
+    int i, j, sum;
+    char ch = '*';
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= 3 * (n - i); j++)
+        {
+            printf(" ");
+        }
+        printf("*");
+        for (sum = 2 * i - 1; sum >= 2; sum--)
+        {
+            printf("%3c", ch);
+        }
+        printf("\n");
+    }
+    for (i = i - 2; i >= 1; i--)
+    {
+        for (j = 1; j <= 3 * (n - i); j++)
+        {
+            printf(" ");
+        }
+        printf("*");
+        for (sum = 2 * i - 1; sum >= 2; sum--)
+        {
+            printf("%3c", ch);
+        }
+        printf("\n");
+    }
+    return 0;
+}

a625b1a4ae942edcc11c6674556574fa1bb828ff
CS-Lawrencium <928397656@qq.com>
2020-11-04 16:56:42 +0800

    dd

--- CodeTest.c
+++ CodeTest.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+int main()
 {
-    
+    int a[5],i;
+    for(i=0;i<=4;i++)
+    {
+        printf("Please enter n:");
+        scanf("%d",&a[i]);
+        if(a[i]<0)
+        {
+            printf("Program is over!\n");
+            break;
+        }
+        else printf("n = %d\n",a[i]);
+    }
+    return 0;
 }
---
+++ RollingString.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<string.h>
+void reverse(char s[])
+{                
+    int len;
+ 
+    len = strlen(s);
+    if (len == 1)
+    {                
+        printf("%c", s[0]);
+    }
+    else
+    {                
+        reverse(s + 1);
+        printf("%c", s[0]);
+    }
+}                
+ 
+int main()
+{                
+    char s[100];
+ 
+    printf("input your string:\n");
+    scanf("%s", s);
+    reverse(s);
+}       
+//反向输出字符串
